f327fd50eb5d05f0586a1e3c7bf137ef7f9721a0
Rui Leite <ruipimentelleite@gmail.com>
2026-07-25 13:54:34 +0000

    Implementa layout Angular Material `purple-green`.

--- apps/front/src/app/components/layout/layout.component.ts
+++ apps/front/src/app/components/layout/layout.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -21,6 +22,7 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
+    RouterModule,
   ]
 })
 export class LayoutComponent {
